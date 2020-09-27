ac76e6e822e67f75062049209d5f4f0abec97892
lilixac <hinewton25@gmail.com>
2020-09-27 21:56:02 +0545

    Crypto arithmetic

---
+++ crypto-arith.py
@@ -0,0 +1,27 @@
+import itertools
+
+def solve(problem):
+	y,w3 = problem.replace(' ','').split('=')
+	w1,w2 = y.split('+')
+
+	variables = set(w3)
+	for i in w1:
+		variables.add(i)
+	for i in w2:
+		variables.add(i)
+
+	domain = range(10)
+	for perm in itertools.permutations(domain, len(variables)):
+		soln = dict(zip(variables, perm))
+
+		def word_value(word):
+			val = [soln[str(i)] for i in word]
+			return sum(d*10**i for i,d in enumerate(val[::-1]))
+
+		if not (soln[w1[0]] == 0 or soln[w2[0]] == 0 or soln[w3[0]] == 0):
+			if word_value(w1) + word_value(w2) == word_value(w3):
+				print("Solution found: ", word_value(w1), " + ", word_value(w2), " = ", word_value(w3))
+				exit(1)
+
+a = input("Enter the string to evaluate: ")
+solve(a)
